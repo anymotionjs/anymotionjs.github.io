59a4f1af1d98ebf028a8adc487585b64f3a42146
Alain <yinxulai@hotmail.com>
2023-10-30 21:42:38 +0800

    WIP

--- source/index.tsx
+++ source/index.tsx
@@ -1,92 +1,53 @@
 import { createApp, onMounted, createRef } from 'airx'
+
+import { use, Tween, TimeProgressDriver, easingFunction } from '@anymotion/core'
 import htmlDriver from '@anymotion/html-driver'
-import anymotion, { Direction, Time } from '@anymotion/core'
 
 import { Action, Card } from './common/card'
 
-anymotion.use(htmlDriver)
+use(htmlDriver)
 
 function App() {
-  let tweenTime: Time | null = null
-  let tweenTimeDirectionIndex = 1
-  let tweenTimeEasingFunctionIndex = 1
-  let tweenTimeDirection = createRef<Direction>('normal')
-  let tweenTimeEasingFunction = createRef(Time.EasingFunction.linear)
+  const time = new TimeProgressDriver()
+  let tweenTimeEasingFunction = createRef(easingFunction.linear)
 
   onMounted(() => {
-    tweenTime = anymotion
-      .to('#test-element', {
-        scale: 1,
-        width: '200px',
-        height: '200px',
-        // rotate: '1.57rad',
-        top: '200px',
-        left: '80px',
-        bottom: '30px',
-        right: '40%',
-        translate: '200px 50% 50rem',
-        backgroundColor: '#3f5efb'
-      })
-      .time
+    time.drive(new Tween('#test-element').to({
+      scale: 1,
+      width: '200px',
+      height: '200px',
+      // rotate: '1.57rad',
+      top: '200px',
+      left: '80px',
+      bottom: '30px',
+      right: '40%',
+      translate: '200px 50% 50rem',
+      backgroundColor: '#3f5efb'
+    }))
       .duration(1000)
       .repeat(Infinity)
-      .direction(tweenTimeDirection.value)
-      .easingFunction(tweenTimeEasingFunction.value)
+      .direction('alternate')
+      .ease(tweenTimeEasingFunction.value)
   })
-
-  const nextDirection = () => {
-    const keys: Direction[] = [
-      'normal',
-      'reverse',
-      'alternate'
-    ]
-
-    tweenTimeDirectionIndex %= keys.length
-    tweenTimeDirection.value = keys[tweenTimeDirectionIndex]
-    tweenTime?.direction(tweenTimeDirection.value)
-    tweenTimeDirectionIndex += 1
-  }
-
-  const nextEasingFunction = () => {
-    const keys = Object.keys(Time.EasingFunction).sort()
-
-    tweenTimeEasingFunctionIndex %= keys.length
-    const key = keys[tweenTimeEasingFunctionIndex]
-    tweenTimeEasingFunction = (Time.EasingFunction as any)[key]
-    tweenTime?.easingFunction(tweenTimeEasingFunction.value)
-    tweenTimeEasingFunctionIndex += 1
-    if (key === 'bezier') nextEasingFunction()
-  }
 
   const actions: Action[] = [
     {
       text: 'paly',
-      onClick: () => tweenTime?.paly()
+      onClick: () => time.play()
     }, {
       text: 'pause',
-      onClick: () => tweenTime?.pause()
-    }, {
-      text: 'next direction',
-      onClick: () => nextDirection()
-    }, {
-      text: 'next easing function',
-      onClick: () => nextEasingFunction()
+      onClick: () => time?.pause()
     }
   ]
 
   return () => (
     <Card title="TEST">
-      <Card
-        title="HTML"
-        actions={actions}
-        states={[tweenTimeDirection.value, tweenTimeEasingFunction.value.name]}
-      >
+      <Card title="HTML" actions={actions}>
         <div id="test-element">
           Motion
         </div>
       </Card>
     </Card>
-
   )
 }
 
